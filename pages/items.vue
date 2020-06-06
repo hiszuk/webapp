81acb9f846626b4331909bce9dd466c5d6f43c9b
Hisakazu KATO <hisakazu.katou@daikin.co.jp>
2020-06-07 03:11:49 +0900

    第2回終了時点

--- pages/items.vue
+++ pages/items.vue
@@ -5,9 +5,27 @@
         <div></div>
       </el-col>
       <el-col :span="20">
-        <div>
-          <h2>Items</h2>
-        </div>
+        <el-input v-model="search" size="small" placeholder="検索文字列を入力">
+          <template slot="prepend">内容で絞り込む</template>
+        </el-input>
+        <el-table ref="itemTable" :data="tableData.filter((data) => !search || data.content.toLowerCase().includes(search.toLowerCase()))" height="400" style="width: 100%">
+          <el-table-column prop="id" label="ID" min-width="40" header-align="center" align="right"> </el-table-column>
+          <el-table-column prop="title" label="タイトル" min-width="200" header-align="center" show-overflow-tooltip> </el-table-column>
+          <el-table-column prop="content" label="内容" min-width="300" header-align="center" show-overflow-tooltip> </el-table-column>
+          <el-table-column prop="status" label="状態" min-width="100" align="center">
+            <template slot-scope="scope">
+              <el-tag :type="scope.row.status === 'DONE' ? 'success' : scope.row.status === 'PROGRESS' ? 'primary' : 'warning'" disable-transitions>
+                {{ scope.row.status }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" min-width="120" header-align="center">
+            <template slot-scope="scope">
+              <el-button size="small" circle @click="handleEdit(scope.$index, scope.row)"><i class="el-icon-edit"></i> </el-button>
+              <el-button size="small" type="danger" circle @click="handleDelete(scope.$index, scope.row)"><i class="el-icon-delete"></i> </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
       </el-col>
       <el-col :span="2">
         <div></div>
@@ -15,3 +33,65 @@
     </el-row>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          id: '1',
+          title: 'WEBApplication作成',
+          content: 'NUXT.js+ElementUIでフロントエンドのWEBアプリを作成する',
+          status: 'DONE'
+        },
+        {
+          id: '2',
+          title: 'RESTfulAPI作成',
+          content: 'SpringBootを用いてRESTfulAPIを構築する',
+          status: 'DONE'
+        },
+        {
+          id: '3',
+          title: 'フロントエンドとバックエンドを結合',
+          content: 'NUXTのアプリからaxios経由でRESTAPIをコールしフロントとバックエンドを繋げる',
+          status: 'PROGRESS'
+        },
+        {
+          id: '4',
+          title: 'MySQLのDockerイメージを作成する',
+          content: 'DockerでRESTfulAPIでアクセスするMySQLのイメージを作成する',
+          status: 'PROGRESS'
+        },
+        {
+          id: '5',
+          title: 'バックエンドのDockerイメージを作成する',
+          content: 'Javaのイメージにバックエンドのjarをレイヤー化したイメージを作成する',
+          status: 'TODO'
+        },
+        {
+          id: '6',
+          title: 'フロントエンドのDockerイメージを作成する',
+          content: 'Node.jsからNUXTをインストールしWEBアプリをコピー後にbuildするイメージを作成する',
+          status: 'TODO'
+        },
+        {
+          id: '7',
+          title: 'Docker-composeでパッケージ化する',
+          content: 'フロントエンド・バックエンド・DBの3層をひとつのパッケージにまとめる',
+          status: 'TODO'
+        }
+      ],
+      search: ''
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    }
+  }
+}
+</script>
