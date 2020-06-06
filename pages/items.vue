c1f800cfa91a7ae0df4286ff42e56b26e89f55e2
Hisakazu KATO <hisakazu.katou@daikin.co.jp>
2020-06-06 11:17:35 +0900

    テーブル追加(Refine)

--- pages/items.vue
+++ pages/items.vue
@@ -6,10 +6,8 @@
       </el-col>
       <el-col :span="20">
         <el-table :data="tableData" height="250" style="width: 100%">
-          <el-table-column prop="date" label="Date" width="180">
-          </el-table-column>
-          <el-table-column prop="name" label="Name" width="180">
-          </el-table-column>
+          <el-table-column prop="date" label="Date" width="180"> </el-table-column>
+          <el-table-column prop="name" label="Name" width="180"> </el-table-column>
           <el-table-column prop="address" label="Address"> </el-table-column>
         </el-table>
       </el-col>
